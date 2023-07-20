3870bf95173e01f5c201d5c721af70908ad974e7
Khoubaib-Boughalmi <khoubaib.kb99@gmail.com>
2023-07-20 02:00:02 +0100

    finished ex06

---
+++ ex06/main.cpp
@@ -0,0 +1,47 @@
+#include "Harl.h"
+
+
+int harlFilterSelector(std::string level)
+{
+    std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+    for (size_t i = 0; i < 4; i++)
+    {
+        if(levels[i] == level)
+            return (i);
+    }
+    return (-1);
+}
+void    harlFilterSwitch(std::string level)
+{
+    Harl harlTheCryBaby;
+    switch (harlFilterSelector(level))
+    {
+    case 0:
+        harlTheCryBaby.complain("DEBUG");
+        harlTheCryBaby.complain("INFO");
+        harlTheCryBaby.complain("WARNING");
+        harlTheCryBaby.complain("ERROR");
+        break;
+    case 1:
+        harlTheCryBaby.complain("INFO");
+        harlTheCryBaby.complain("WARNING");
+        harlTheCryBaby.complain("ERROR");
+        break;    
+    case 2:
+        harlTheCryBaby.complain("WARNING");
+        harlTheCryBaby.complain("ERROR");
+        break;    
+    case 3:
+        harlTheCryBaby.complain("ERROR");
+        break;    
+    default:
+        std::cout << "[ Probably complaining about insignificant problems ]";
+        break;
+    }
+}
+
+int main()
+{
+    harlFilterSwitch("DEBUG");
+    return (0);
+}
